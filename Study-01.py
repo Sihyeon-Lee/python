0d4e75532d4bcebeeab0fefbc171a95120b74aab
Leesihyeon <83231521+Sihyeon-Lee@users.noreply.github.com>
2024-01-02 12:32:31 +0900

    Study-01.py

--- Study-01.py
+++ Study-01.py
@@ -3,8 +3,8 @@ import turtle
 
 turtle.shape('turtle')
 
-turtle.forward(200)
-turtle.right(90)
+turtle.forward(200) # 200 직진
+turtle.right(90) # 90도 오른쪽으로 회전
 turtle.forward(200)
 turtle.right(90)
 turtle.forward(200)
