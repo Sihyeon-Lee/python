81100b1973cc2ae38651a6df6088a2c4c623305c
Leesihyeon <83231521+Sihyeon-Lee@users.noreply.github.com>
2024-01-25 21:27:33 +0900

    code10-16.py

--- code10-16.py
+++ code10-16.py
@@ -7,6 +7,7 @@ from tkinter import messagebox
 def keyEvent(event) :
     messagebox.showinfo("키보드 이벤트", "눌린 키" +
                         chr(event.keycode))
+                        #대소문자 다시 확인해보기
 
 # 메인 코드 부분
 window = Tk()
